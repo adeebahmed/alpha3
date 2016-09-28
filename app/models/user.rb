37a6d387330d33c36764a5fbec0ab223f303f72e
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-09-28 14:28:57 -0500

    Added User test;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :weight, presence: true
+  validates :height, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "User must have firstname" do
+    user1 = User.new
+    assert user.invalid?
+  end
+
 end
