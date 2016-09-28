540a91e74088ef8a3c53eb539f0d683a42bab40a
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-09-28 17:26:50 -0500

    Ran foreign key migration on User model;
      $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927012337) do
+ActiveRecord::Schema.define(version: 20160928213328) do
 
   create_table "liftdata", force: :cascade do |t|
     t.string   "exercise"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160927012337) do
     t.integer  "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_liftdata_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
