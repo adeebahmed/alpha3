51fff12bfb879b24bdf6182f81d8a8f7d844e149
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-09-28 21:35:31 -0500

    Added seed data for User and Liftdata models;

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+User.destroy_all
+Liftdata.destroy_all
+
+(1..5).each do |i|
+  user = User.create(firstname: "Adeeb#{i}", lastname: "Ahmed#{i}", height: "70", weight:"175", age:"21)
+ 
+  (1..10).each do |j|
+    liftdata = Liftdata.create(exercise: "exercise no: #j", sets: j, reps: j*2, weight: j*100)  
+  end
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
