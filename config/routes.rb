7b77f6e0462df8425a9e1d41929789b2041c6772
Eric <jr.jv12@gmail.com>
2016-12-05 21:27:37 -0600

    Added named route to authenticate users

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/auth/:provider/callback', to: 'sessions#create'
+
+  get '/auth/:provider', to: lambda{|env| [404, {}, ["Not Found"]]}, as: 'auth'
 end
