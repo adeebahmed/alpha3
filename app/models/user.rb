61e4a87efaf2a5567c20b311eaa9842d8f959268
Eric <jr.jv12@gmail.com>
2016-12-05 21:50:16 -0600

    Added controller and model code to build user records from auth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
   validates :height, presence: true
   
   has_many :liftdatum
+
+  def self.from_omniauth(hash)
+  	user = User.find_or_create_by(uid: hash['uid'], provider: hash['provider'])
+  	user.name = hash['info']['name']
+  	user.nickname = hash['info']['nickname']
+  	user.location = hash['info']['location']
+  	user.image_url = auth_hash['info']['image']
+    user.url = auth_hash['info']['urls']['Twitter']
+  	user.save!
+  	user
+  end
 end
