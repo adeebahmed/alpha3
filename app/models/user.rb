2cf42a5901fe57fcdc3127e72e7c8a6d8e34ca0a
Eric <jr.jv12@gmail.com>
2016-12-08 00:04:29 -0600

    Added default values when logging in

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,12 @@ class User < ApplicationRecord
   	user.location = hash['info']['location']
   	user.image_url = hash['info']['image']
     user.url = hash['info']['urls']['Twitter']
+    user.firstname = "Change me"
+    user.lastname = "Change me"
+    user.weight = "100"
+    user.height = "100"
+    user.age = "21"
+    user.date = Date.today
   	user.save(:validate => false)
   	user
   end
