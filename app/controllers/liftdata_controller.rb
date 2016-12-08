bca9645d1bb477c3bd12cbd0100ea6f6f17fb441
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2016-12-07 19:58:58 -0600

    Liftdatum view now displays who the data belongs to;

--- app/controllers/liftdata_controller.rb
+++ app/controllers/liftdata_controller.rb
@@ -69,6 +69,6 @@ class LiftdataController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def liftdatum_params
-      params.require(:liftdatum).permit(:exercise, :set, :rep, :weight)
+      params.require(:liftdatum).permit(:exercise, :set, :rep, :weight, :user)
     end
 end
--- app/models/liftdatum.rb
+++ app/models/liftdatum.rb
@@ -1,3 +1,4 @@
 class Liftdatum < ApplicationRecord
   belongs_to :user
+  delegate :firstname, to: :user, prefix: true #-> @liftdatum.user_name
 end
