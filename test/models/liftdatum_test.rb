075f4d55b41ecd272095480934285d4604cf43a8
Eric <jr.jv12@gmail.com>
2016-12-07 23:54:13 -0600

    added liftdatum test

--- test/models/liftdatum_test.rb
+++ test/models/liftdatum_test.rb
@@ -5,9 +5,14 @@ class LiftdatumTest < ActiveSupport::TestCase
   #   assert true
   # end
   
-  # test "liftdata must have a user" do
-  #   liftdata = liftdata(:liftdata_one)
-  #   assert_equal liftdata.user.firstname, "user1"
-  # end
+	test "liftdata must have a user" do
+	liftdata = liftdata(:liftdata_one)
+	assert_equal liftdata.user.firstname, "user_1"
+  end
+  
+  test "liftdata must have weight data" do
+  	liftdata = liftdata(:lift_one)
+  	assert liftdata.valid?
+  end
 
 end
